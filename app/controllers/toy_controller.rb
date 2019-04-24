2693533b0ca9335eed1b30f064c10029468cc987
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-04-24 12:43:59 -0400

    finished sinatra project

--- app/controllers/toy_controller.rb
+++ app/controllers/toy_controller.rb
@@ -1,8 +1,19 @@
 class ToyController < Sinatra::Base
 
   set :views, 'app/views/toy'
+
   get '/toys' do
     @toys = Toy.all
     erb :index
   end
+
+  get '/toys/new' do
+    erb :new
+  end
+
+  post '/toys' do
+    toy = Toy.find_or_create_by(name: params[:name], dog: Dog.find(params[:dog]))
+    redirect "/dogs/#{params[:dog]}"
+  end
+
 end
