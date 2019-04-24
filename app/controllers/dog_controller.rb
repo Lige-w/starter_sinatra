5437a03580b081d8a88f0811231aa6826a78c75f
github username <github email address>
2019-04-24 12:13:50 -0400

    built dog controller, started toy controller

--- app/controllers/dog_controller.rb
+++ app/controllers/dog_controller.rb
@@ -6,4 +6,19 @@ class DogController < Sinatra::Base
     @dogs = Dog.all
     erb :index
   end
+
+  get '/dogs/new' do
+    erb :new
+  end
+
+  get '/dogs/:id' do
+    @dog = Dog.find(params[:id])
+    erb :show
+  end
+
+  post '/dogs' do
+    dog = Dog.find_or_create_by(name: params[:name],
+                                breed: params[:breed])
+    redirect "/dogs/#{dog.id}"
+  end
 end
--- app/controllers/toy_controller.rb
+++ app/controllers/toy_controller.rb
@@ -1,3 +1,8 @@
 class ToyController < Sinatra::Base
 
+  set :views, 'app/views/toy'
+  get '/toys' do
+    @toys = Toy.all
+    erb :index
+  end
 end
